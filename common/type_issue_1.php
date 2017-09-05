c8c2ee887857a389e5aab132abce203678ac99b8
boyoon <boyoon8@gmail.com>
2017-09-05 10:24:02 +0900

    init

---
+++ common/type_issue_1.php
@@ -0,0 +1,86 @@
+<?php
+if($MD=='topstarnews'){
+$sCode = "071p"; //pv 체크
+$pageid1 = "06zm" ; // 실시간 정보
+$pageid2 = "0704" ; // 관심기사
+$pageid3 = "070M" ; // 중단배너
+$pageid4 = "070e" ; // 이슈포토
+$pageid5 = "070w" ; // 베스트 생활정보
+$pageid6 = "071E" ; // 금주의 핫클릭
+$pageid7 = "071W" ; // 하단배너
+$post_id='86';
+$popstate='disable';
+$press_url='http://issuepopcon.com/?MD=topstarnews';
+$view_type='random';
+}
+if($MD=='fnews'){
+$sCode = "071m"; //pv 체크
+$pageid1 = "06zk" ; // 실시간 정보
+$pageid2 = "0702" ; // 관심기사
+$pageid3 = "070K" ; // 중단배너
+$pageid4 = "070c" ; // 이슈포토
+$pageid5 = "070u" ; // 베스트 생활정보
+$pageid6 = "071C" ; // 금주의 핫클릭
+$pageid7 = "071U" ; // 하단배너
+$post_id='85';
+$popstate='disable';
+$press_url='http://issuepopcon.com/?MD=fnews';
+$view_type='random';
+}
+if($MD=='heraldpop'){
+$sCode = "071q"; //pv 체크
+$pageid1 = "06zn" ; // 실시간 정보
+$pageid2 = "0705" ; // 관심기사
+$pageid3 = "070N" ; // 중단배너
+$pageid4 = "070f" ; // 이슈포토
+$pageid5 = "070x" ; // 베스트 생활정보
+$pageid6 = "071F" ; // 금주의 핫클릭
+$pageid7 = "071X" ; // 하단배너
+$post_id='84';
+$popstate='disable';
+$press_url='http://issuepopcon.com/?MD=heraldpop';
+$view_type='random';
+}
+if($MD=='jemin'){
+$sCode = "071o"; //pv 체크
+$pageid1 = "06zl" ; // 실시간 정보
+$pageid2 = "0703" ; // 관심기사
+$pageid3 = "070L" ; // 중단배너
+$pageid4 = "070d" ; // 이슈포토
+$pageid5 = "070v" ; // 베스트 생활정보
+$pageid6 = "071D" ; // 금주의 핫클릭
+$pageid7 = "071V" ; // 하단배너
+$post_id='83';
+$popstate='disable';
+$press_url='http://issuepopcon.com/?MD=jemin';
+$view_type='random';
+}
+if($MD=='newstown'){
+$sCode = "071j"; //pv 체크
+$pageid1 = "06zX" ; // 실시간 정보
+$pageid2 = "06zp" ; // 관심기사
+$pageid3 = "0707" ; // 중단배너
+$pageid4 = "070P" ; // 이슈포토
+$pageid5 = "070h" ; // 베스트 생활정보
+$pageid6 = "070z" ; // 금주의 핫클릭
+$pageid7 = "071H" ; // 하단배너
+$post_id='82';
+$popstate='disable';
+$press_url='http://issuepopcon.com/?MD=newstown';
+$view_type='random';
+}
+if($MD=='moodeung'){
+$sCode = "071k"; //pv 체크
+$pageid1 = "06zY" ; // 실시간 정보
+$pageid2 = "06zq" ; // 관심기사
+$pageid3 = "0708" ; // 중단배너
+$pageid4 = "070Q" ; // 이슈포토
+$pageid5 = "070i" ; // 베스트 생활정보
+$pageid6 = "0710" ; // 금주의 핫클릭
+$pageid7 = "071I" ; // 하단배너
+$post_id='81';
+$popstate='disable';
+$press_url='http://issuepopcon.com/?MD=moodeung';
+$view_type='random';
+}
+?>
---
+++ issuepopcon02/common/type_issue_1.php
@@ -0,0 +1,41 @@
+<?php
+if($MD=='newstown'){
+$sCode = "071j"; //pv 체크
+$pageid1 = "06zX" ; // 실시간 정보
+$pageid2 = "06zp" ; // 관심기사
+$pageid3 = "0707" ; // 중단배너
+$pageid4 = "070P" ; // 이슈포토
+$pageid5 = "070h" ; // 베스트 생활정보
+$pageid6 = "070z" ; // 금주의 핫클릭
+$pageid7 = "071H" ; // 하단배너
+$post_id='82';
+$popstate='disable';
+$press_url='http://issuepopcon.com/?MD=newstown';
+$view_type='random';
+}
+if($MD=='moodeung'){
+$sCode = "071k"; //pv 체크
+$pageid1 = "06zY" ; // 실시간 정보
+$pageid2 = "06zq" ; // 관심기사
+$pageid3 = "0708" ; // 중단배너
+$pageid4 = "070Q" ; // 이슈포토
+$pageid5 = "070i" ; // 베스트 생활정보
+$pageid6 = "0710" ; // 금주의 핫클릭
+$pageid7 = "071I" ; // 하단배너
+$post_id='81';
+$popstate='disable';
+$press_url='http://issuepopcon.com/?MD=moodeung';
+$view_type='random';
+}
+if($MD=='default'){
+$sCode = "0676"; //pv 체크
+$post_id='77';
+$popstate='enable';
+$press_url='http://issuepopcon.com/?MD=default';
+$view_type='random';
+$post_link[0]['pln_start']=0;
+$post_link[0]['pln_end']=0;
+$post_link[0]['pln_url']='http://naver.com';
+$post_link[0]['pln_id']='54';
+}
+?>
